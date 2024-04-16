b7d20682c89ae1660a5352163f84f9f76580da45
eugenekigen <Kiplagateugene45@gmail.com>
2024-04-16 01:55:11 -0700

    first commit

---
+++ Demo3/script.js
@@ -0,0 +1,23 @@
+//Numbers:
+let length = 18;
+let currency = 134.16;
+
+//Strings:
+let color = "Red";
+let firtname = "Eugene";
+
+//Booleans:
+let x = true;
+let y = false;
+
+//Char:
+let name = 'a';
+
+//object:
+const person = {firstName: "Eugene", lastname: "Kiplagat"};
+
+//Array object:
+const cars = ["Toyota", "Subaru", "Mercedes"];
+
+//Date object:
+const date = new Date("2024-04-02");
